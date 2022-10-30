7b81dae8c3b566cd4b49b2b85a4a30fe5c8c2d7d
Ciro García Belmonte <ciro.garcia.belmonte@gmail.com>
2022-10-30 12:10:57 +0100

    Updated imports

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,17 @@
 import {createApp} from 'vue'
+import {BootstrapVue3} from 'bootstrap-vue-3'
+import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
 import App from './App.vue'
-import "bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-icons/font/bootstrap-icons.css"
 import './assets/css/main.css'
 import router from "./router"
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(BootstrapVue3)
+app.use(BootstrapIconsPlugin)
 
 app.mount('#app')
